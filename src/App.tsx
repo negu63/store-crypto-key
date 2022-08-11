20ce3dddadd64776589f0e7b3db08ef2822ee199
negu63 <negu6633@gmail.com>
2022-08-12 03:21:36 +0900

    Add plain text box and state

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,8 @@
 import { db } from "./db";
+import { useState } from "react";
 
 function App() {
+  const [text, setText] = useState("");
   async function generateAESKey() {
     const key = await crypto.subtle.generateKey(
       { name: "AES-CBC", length: 256 },
@@ -13,22 +15,16 @@ function App() {
   }
 
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <label htmlFor="plainText">
+        <div>Plain Text</div>
+        <input
+          type="text"
+          name="plainText"
+          id="plainText"
+          value={text}
+          onChange={(e) => setText(e.target.value)}
+        />
+      </label>
   );
 }
 
