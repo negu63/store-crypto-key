9763a3394441fa807f2e98169f0707d90bfc2221
negu63 <negu6633@gmail.com>
2022-08-12 03:25:43 +0900

    Add encrypt with AES and state

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,12 @@
 import { db } from "./db";
 import { useState } from "react";
+import { Buffer } from "buffer";
+const encoder = new TextEncoder();
+const iv = crypto.getRandomValues(new Uint8Array(16));
 
 function App() {
   const [text, setText] = useState("");
+  const [cryptogram, setCryptogram] = useState("");
   async function generateAESKey() {
     const key = await crypto.subtle.generateKey(
       { name: "AES-CBC", length: 256 },
@@ -14,6 +18,18 @@ function App() {
     await db.key.add({ key });
   }
 
+  async function encryptWithAESKey() {
+    const key = (await db.key.limit(1).toArray())[0].key as CryptoKey;
+    const encrypted: ArrayBuffer = await crypto.subtle.encrypt(
+      {
+        name: "AES-CBC",
+        iv,
+      },
+      key,
+      encoder.encode(text)
+    );
+    setCryptogram(Buffer.from(new Uint8Array(encrypted)).toString("base64"));
+  }
   return (
       <div>
         <input
